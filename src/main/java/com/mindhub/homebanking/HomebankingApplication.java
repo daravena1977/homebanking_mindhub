c8338bb4eca77a65f08cb9c887cfe77da8827c69
Diego <disorder1977@gmail.com>
2023-08-03 21:43:30 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,31 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args) -> {
+			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
+			Client client2 = new Client("Diego", "Aravena", "daravena@mindhub.com");
+			Client client3 = new Client("Enzo", "Aravena", "earavena@mindhub.com");
+			Client client4 = new Client("Sandra", "Lazaro", "sandra.lazaro@mindhub.com");
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+			clientRepository.save(client3);
+			clientRepository.save(client4);
+		};
+	}
+
+}
